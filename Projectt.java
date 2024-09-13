85f5031c8f9c0d0956c58a8ddea799dcef8ea538
Preetha A <51926292+p-reetha@users.noreply.github.com>
2024-09-13 21:18:26 +0530

    Add files via upload

---
+++ Projectt.java
@@ -0,0 +1,18 @@
+package week5;
+
+import java.util.HashMap;
+
+enum course {
+	Java, Python, JS, SQL
+}
+
+public class Projectt {
+
+	public static void main(String[] args) {
+		HashMap<String, Integer> courseDetails = new HashMap<>();
+		courseDetails.put("Java", 5);
+		courseDetails.put("Python", 4);
+		courseDetails.put("Js", 6);
+		}
+
+}
